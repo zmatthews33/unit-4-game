707b637bde8d989a63be41a893c0ca7c0496e331
whatevermane <zmatt33@gmail.com>
2019-02-20 13:40:12 -0600

    Created characters as objects with HP and damage

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+    var characters = {
+        "byow": {
+            name: "Dr. Byow",
+            hp: 160,
+            damage: 13
+        },
+        "dusty": {
+            name: "Dusty Phoenix",
+            hp: 130,
+            damage: 10
+        },
+        "heavy": {
+            name: "Heavy Duty",
+            hp: 170,
+            damage: 12,
+        },
+        "lucky": {
+            name: "Lucky Day",
+            hp: 150,
+            damage: 14,
+        }
+    }
